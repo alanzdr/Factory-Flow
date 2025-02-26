cf43c2f838bb35f63b3f602c96a26edc0e76a6bd
AlandersonRosa-FUB <arosa@futurebrand.com>
2025-02-26 16:35:23 -0300

    feat: update SEO

--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 import Home from "@/layouts/Home";
+import { getMetadata } from "@/utils/seo";
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  try {
+    return getMetadata("page", {
+      title: `Create automated workflows`,
+      description:
+        "Factory Flow is a library designed to streamline the creation of automated robotic workflows. It is designed to be simple to use and to scale to any numbers of workflows.",
+      path: `/docs`,
+    });
+  } catch {
+    return {};
+  }
+}
 
 const Page: React.FC = () => {
   return <Home />;
