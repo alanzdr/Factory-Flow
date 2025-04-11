5e0c1fb41a91f6e60199d2bf34bf8a0c7ad38e60
AlandersonRosa-FUB <arosa@futurebrand.com>
2025-04-11 14:31:49 -0300

    feat: move structures

--- docs/mdx-components.tsx
+++
@@ -1,9 +0,0 @@
-import type { MDXComponents } from "mdx/types";
-import Link from "next/link";
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...components,
-    a: Link,
-  };
-}
